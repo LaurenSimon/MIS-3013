3cebc6abd68c32dd3bbc2174f685ea11776f19cf
Lauren Simon <laurensimon25@ou.edu>
2020-09-04 16:11:39 -0500

    Created and finished Participation 2 -Sum of 3

---
+++ InClassExamples/ConditionalStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = 0;
+            double distance = 0;
+            string response = "";
+            bool containsHazardous = false;
+
+            Console.WriteLine("What is the weight of your shipment, in pounds? >>")
+            response = Console.ReadLine();
+            weight = Convert.ToDouble(response);
+
+            Console.WriteLine("What is the distance of your shipment, in pounds? >>")
+            distance = Convert.ToDouble(Console.ReadLine);
+
+            Console.WriteLine("Does your shipment contain in pounds? >>")
+            response = Console.ReadLine();
+
+            if (response == "yes")
+            {
+                containsHazardous = true;
+            }
+            else if (response == "no")
+        }
+                containsHazardous = false;
+    }
+}
---
+++ InClassExamples/InputOutput/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static const string CLASS_NAME = "MIS 3013";
+        static void Main(string[] args)
+        {
+            //This won't work because it is a constant
+            // CLASS_NAME = "MIS 3353"
+            //string firstName, lastName;
+            string firstName = "Lauren";
+            string lastName = "Simon";
+            Console.WriteLine(firstName + " " + lastName);
+                " is teaching " + CLASS_NAME);
+        }
+    }
+}
