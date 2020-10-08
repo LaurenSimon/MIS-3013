1b0c00b260c37b9abab60dcb5584da6c2ae92531
Lauren Simon <laurensimon25@ou.edu>
2020-10-07 20:26:00 -0500

    started participations

---
+++ Participations/Collections Min,Max,Avg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Min_Max_Avg
+{
+    class Program
+    {
+        // Ask the user to enter all of their exam grades.  Once they are done, calculate the minimum score, 
+        //  the maximum score and the average score for all of their scores.  Perform this using at least 2 Loops 
+        // (can be the same type of loop) and not any built in functions.
+        // EXTRA: Calculate the Mode of the exam scores.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Participations/Processing a CSV File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processing_a_CSV_File
+{
+    class Program
+    {
+        // Create a Console application that will read (File.ReadAllLines) in the sales_data_sample.csv . 
+        // We want to sum up all of the sales with the status of Shipped and output the total sales by year 
+        // (e.g. how much did we sell in 2003, 2004, 2005) as well as total sales by month across all years 
+        // (e.g. how much did we sell in January of 2003/2004/2005 and February of 2003/2004/2005 etc.), 
+        // make sure to output it as January/February etc and not 1/2.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
