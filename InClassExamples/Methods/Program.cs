9ab75beb90d28b341cbb1b6c481d9ff94601aca2
Lauren Simon <laurensimon25@ou.edu>
2020-10-23 13:08:10 -0500

    worked on (P) methods

--- InClassExamples/Methods/Program.cs
+++ InClassExamples/Methods/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Markup;
 
 namespace Methods
 {
     class Program
     {
+        static List<int> values = new List<int>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Random rand = new Random(); // this is an instance
+
+            Console.ReadKey();
+        }
+
+        static bool PossiblyAddStringToAList(string inputFromWhoeverCalledMe)
+        {
+            bool wasSuccess = true;
+            int value;
+
+            wasSuccess = int.TryParse(inputFromWhoeverCalledMe, out value);
+
+            if (wasSuccess)
+            {
+                // we want to add it to the list
+                Values.Add(value);
+            }
+            else
+            {
+                // we want to tell them they had an invalid input
+                Console.WriteLine($"{value} was not a valid integer. You must enter an integer");
+            }
+
+            return wasSuccess;
         }
     }
 }
---
+++ Participations/Methods - Simple Method/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methods___Simple_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+
+        static string Speak (string Sound)
+        {
+            string Dog = ();
+            string Monkey = ();
+            Console.WriteLine("Enter a type of animal.");
+            string Animal = Console.ReadLine();
+
+            if (Animal = "dog")
+            {
+                Console.WriteLine("BARK BARK");
+            }
+            else if (Animal = "monkey")
+            {
+                Console.WriteLine("GRUNTING");
+            }
+            else if (Animal = "goat")
+            {
+                Console.WriteLine("BLEAT");
+            }
+            else
+            {
+                // INVALID
+            }
+            Console.ReadKey();
+            // i rewatched the lectures about how to do methods and I am still very confused.
+            // i am hoping to get clarity on this in class tuesday!
+        }
+    }
+}
