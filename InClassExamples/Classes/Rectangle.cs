b7d04f5951e452ec0ae44491eec6efe03cdbc81d
Lauren Simon <laurensimon25@ou.edu>
2020-10-29 12:32:31 -0500

    did in class example

---
+++ InClassExamples/Classes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+
+        /// <summary>
+        /// default/empty constructor
+        /// </summary>
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+        }
+
+        /// <summary>
+        ///  overload constructor
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+
+        public double Area()
+        {
+            double result = width * height;
+
+            return result;
+        }
+    }
+}
