4d8f296b6ce7025b6914b6ffe1e8982a39f6c338
Lauren Simon <laurensimon25@ou.edu>
2020-11-30 17:46:55 -0600

    worked on participation

---
+++ Participations/(P) Classes - TextBox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _P__Classes___TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create at least two TextBox instances with different values set for the properties.  
+            // Call the DisplayText method on each instance of your TextBox.
+            TextBox TextBox1 = new TextBox()
+            {
+                Console.WriteLine("Text Box Number 1");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                // Console.WriteLine(DisplayText);
+            }
+
+            TextBox TextBox2 = new TextBox()
+            {
+                Console.WriteLine("Text Box Number 2");
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                // Console.WriteLine(DisplayText);
+            }
+        }
+
+        // static void DisplayText (string ForeGroundColor)
+        // {
+        // }
+
+        // static void DisplayText (string BackgroundColor)
+        // {
+        // }
+    }
+}
---
+++ Participations/(P) Classes - TextBox/TextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__Classes___TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        // Padding is the spacing before and after text (hint: add the correct number of spaces 
+        // BEFORE and AFTER the test property.
+
+        public ConsoleColor BackColor { get; set; }
+
+        public ConsoleColor ForeColor { get; set; }
+
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+
+        }
+
+        public void DisplayText()
+        {
+            // Writes out on the console the text with the correct ForegroundColor and BackgroundColor associated with Back/ Fore color 
+            // property of the instance. There should be padding before / after the Text property based on the int value associated with padding
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            string BackgroundColor;
+            string ForegroundColor;
+            Console.WriteLine(BackgroundColor.PadLeft(10, '-'));
+            Console.WriteLine(BackgroundColor.PadRight(10, '-'));
+            Console.WriteLine(ForegroundColor.PadRight(10, '-'));
+            Console.WriteLine(ForegroundColor.PadLeft(10, '-'));
+            // used pad method from referencing 
+            // https://docs.microsoft.com/en-us/dotnet/standard/base-types/padding
+        }
+    }
+}
